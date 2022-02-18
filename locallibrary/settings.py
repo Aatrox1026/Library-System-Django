d735756965e02f1febbd7a85b80ef11c21d19d18
Aatrox <t107590043@ntut.org.tw>
2022-02-18 16:00:46 +0800

    update

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4^(6vl1d-j$gf7-yz5v#z74t7u^7@k%(dv_10!0aw)k4s3#!ur'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
