51c37ec3172ca26a36f53f15f5b809fcb57df146
Aatrox <t107590043@ntut.org.tw>
2022-02-18 15:39:02 +0800

    update

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -74,23 +74,23 @@ WSGI_APPLICATION = 'locallibrary.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'locallibrary',
-        'USER': 'Aatrox',
-        'PASSWORD': 'knnn4848',
-        'HOST': '',
-        'PORT': ''
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'locallibrary',
+#         'USER': 'Aatrox',
+#         'PASSWORD': 'knnn4848',
+#         'HOST': '',
+#         'PORT': ''
+#     }
+# }
 
 
 # Password validation
